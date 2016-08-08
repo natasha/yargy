e7c88ac12d58bf5d28741ca8f41ec5d2b4482252
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-08-09 00:50:32 +0300

    Add dictionary label

--- yargy/labels.py
+++ yargy/labels.py
@@ -4,7 +4,11 @@ def gram_label(token, value, stack):
 def gram_not_label(token, value, stack):
     return not value in token.grammemes
 
+def dictionary_label(token, values, stack):
+    return any((n in values) for n in token.forms)
+
 LABELS_LOOKUP_MAP = {
     "gram": gram_label,
     "gram-not": gram_not_label,
+    "dictionary": dictionary_label,
 }
