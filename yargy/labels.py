309ca05365e205ea2a31d4647b0f836679ebe9b9
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-08-09 00:06:53 +0300

    Add gram-not label

--- yargy/labels.py
+++ yargy/labels.py
@@ -1,6 +1,10 @@
 def gram_label(token, value, stack):
     return value in token.grammemes
 
+def gram_not_label(token, value, stack):
+    return not value in token.grammemes
+
 LABELS_LOOKUP_MAP = {
     "gram": gram_label,
+    "gram-not": gram_not_label,
 }
