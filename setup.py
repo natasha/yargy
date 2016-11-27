dc30dc408b81d0715d72325c14d874ea4e7a07f4
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-11-27 06:01:02 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIREMENTS = [
 
 setup(
     name='yargy',
-    version='0.4.3',
+    version='0.4.4',
     description='Tiny rule-based facts extraction package',
     url='https://github.com/bureaucratic-labs/yargy',
     author='Dmitry Veselov',
