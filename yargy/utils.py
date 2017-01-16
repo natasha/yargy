81c567e4be0d3c2f91d3cfa3d04b0b738859da6a
Dmitry Veselov <d.a.veselov@yandex.ru>
2017-01-17 01:17:01 +0300

    Add get_tokens_position and decode_roman_number functions

--- yargy/utils.py
+++ yargy/utils.py
@@ -4,12 +4,44 @@ from itertools import count, takewhile
 def frange(start, stop, step):
     return takewhile(lambda x: x <= stop, (start + i * step for i in count()))
 
+def get_tokens_position(tokens):
+    if not tokens:
+        return None
+    head, tail = tokens[0], tokens[-1]
+    return head.position[0], tail.position[1]
+
 def get_original_text(text, tokens):
     '''
     Returns original text captured by parser grammars
     '''
-    if not tokens:
+    position = get_tokens_position(tokens)
+    if not position:
         return None
-    head, tail = tokens[0], tokens[-1]
-    start, end = head.position[0], tail.position[1]
+    else:
+        start, end = position
     return text[start:end]
+
+# stealed from rosettacode
+ROMAN_VALUES = (
+    ('I', 1),
+    ('IV', 4),
+    ('V', 5),
+    ('IX', 9),
+    ('X', 10),
+    ('XL', 40),
+    ('L', 50),
+    ('XC', 90),
+    ('C', 100),
+    ('CD', 400),
+    ('D', 500),
+    ('CM', 900),
+    ('M', 1000),
+)
+ 
+def decode_roman_number(number):
+    total = 0
+    for symbol, value in reversed(ROMAN_VALUES):
+        while number.startswith(symbol):
+            total += value
+            number = number[len(symbol):]
+    return total
