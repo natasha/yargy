3bf7d2d7eafc1bee863068ea546f6d39abe70ddf
Dima Veselov <d.a.veselov@yandex.ru>
2017-08-25 14:31:32 +0300

    Test that tokenizer doen't drop unknown symbols (closes #22)

---
+++ yargy/tests/test_tokenizer.py
@@ -0,0 +1,14 @@
+import pytest
+
+from yargy.tokenizer import Tokenizer
+
+
+@pytest.fixture
+def tokenizer():
+    return Tokenizer()
+
+
+def test_that_tokenizer_doesnt_drop_symbols(tokenizer):
+    tokens = list(tokenizer('Ханты – Мансийского'))
+    assert [t.value for t in tokens] == ['Ханты', '–', 'Мансийского']
+    assert tokens[1].forms[0].grammemes == {'OTHER'}
