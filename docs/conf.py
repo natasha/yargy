582e0d1b39e7c78e022c5bb147622ea8d21c9e8b
Dima Veselov <d.a.veselov@yandex.ru>
2017-08-26 12:04:05 +0300

    Try to fix docs encoding

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,15 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
 import sys
 
 sys.path.append('..')
 
 source_suffix = '.rst'
+source_encoding= 'utf-8'
 master_doc = 'index'
+
+language='ru'
 
 html_theme = 'alabaster'
 
