4be47872c09281a83a44c54215ff8ce943a8f9f9
Kukushkin Alexander <alex@alexkuk.ru>
2017-09-25 13:30:00 +0300

    Add Fact.key.normalizer(function) feature

--- yargy/interpretation/fact.py
+++ yargy/interpretation/fact.py
@@ -22,28 +22,30 @@ def same_as(item, other):
 
 
 def normalize(item):
-    from .interpretator import Chain
+    from .interpretator import is_chain
 
-    if isinstance(item, Chain):
-        if item.value is not None:
-            return item.value
-
+    if is_chain(item):
         items = item.items
+        normalizer = item.normalizer
         if len(items) == 1:
             item = items[0]
             if is_fact(item):
-                return item.normalized
+                value = item.normalized
             else:
-                return item.value
+                value = item.value
         else:
             if any(is_fact(_) for _ in items):
-                return [
+                value = [
                     (_.normalized if is_fact(_) else _.value)
                     for _ in items
                 ]
             else:
-                return join_tokens(items)
+                value = join_tokens(items)
+        if normalizer:
+            value = normalizer(value)
+        return value
     else:
+        # NOTE is_fact?
         return item
 
 
@@ -51,6 +53,30 @@ def serialize(item):
     if is_fact(item):
         return item.as_json
     return item
+
+
+def spans(item):
+    from .interpretator import is_chain
+    from yargy.token import get_tokens_span
+
+    if is_chain(item):
+        chunks = []
+        chunk = []
+        for item in item.items:
+            if is_fact(item):
+                for span in item.spans:
+                    yield span
+                if chunk:
+                    chunks.append(chunk)
+            else:
+                chunk.append(item)
+        if chunk:
+            chunks.append(chunk)
+        for chunk in chunks:
+            yield get_tokens_span(chunk)
+    elif is_fact(item):
+        for span in item.spans:
+            yield span
 
 
 class Fact(Record):
@@ -125,6 +151,18 @@ class Fact(Record):
                 value = serialize(value)
             data[key] = value
         return data
+
+    @property
+    def spans(self):
+        for key in self.__attributes__:
+            value = getattr(self, key)
+            if key in self.__repeatable:
+                for item in value:
+                    for span in spans(item):
+                        yield span
+            else:
+                for span in spans(value):
+                    yield span
 
     def __repr__(self):
         name = self.__class__.__name__
