059170adc997bb3dd67f6fd7bd165854490ed41f
Alexander Kukushkin <alex@alexkuk.ru>
2023-07-24 12:53:35 +0300

    Patch pymorphy to support py 3.11

--- yargy/morph.py
+++ yargy/morph.py
@@ -6,6 +6,24 @@ from pymorphy2 import MorphAnalyzer as PymorphyAnalyzer
 from .record import Record
 
 
+def pymorphy2_311_hotfix():
+    # https://github.com/pymorphy2/pymorphy2/issues/160#issuecomment-1486657176
+
+    from inspect import getfullargspec
+    from pymorphy2.units.base import BaseAnalyzerUnit
+
+    def _get_param_names_311(klass):
+        if klass.__init__ is object.__init__:
+            return []
+        args = getfullargspec(klass.__init__).args
+        return sorted(args[1:])
+
+    setattr(BaseAnalyzerUnit, '_get_param_names', _get_param_names_311)
+
+
+pymorphy2_311_hotfix()
+
+
 class Gender(Record):
     __attributes__ = ['male', 'female', 'neutral', 'bi', 'general']
 
